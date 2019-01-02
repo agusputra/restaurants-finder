ba2984819d0915b7bb5d4ea28052460bcbffa519
agusputra <agus.putra99@gmail.com>
2019-01-02 13:08:29 +0700

    better submit handler

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,7 @@ import { searchRestaurants } from '../redux/actions'
 const Header = ({ dispatch }) => {
   const keywordInput = React.createRef()
 
-  function search(e) {
-    e.preventDefault()
-    e.stopPropagation()
-    
+  function search() {
     const keyword = keywordInput.current.value
     dispatch(searchRestaurants({ keyword }))
   }
@@ -18,9 +15,9 @@ const Header = ({ dispatch }) => {
       <nav className="navbar navbar-dark bg-dark">
         <a className="navbar-brand" href="/">Restaurants Finder</a>
       </nav>
-      <form className="form-inline my-2">
+      <form className="form-inline my-2" onSubmit={e => e.preventDefault()}>
         <input className="form-control" ref={keywordInput} placeholder="keyword" />
-        <button type="button" className="btn btn-secondary ml-2" onClick={(e) => search(e)}>Search</button>
+        <button className="btn btn-secondary ml-2" onClick={e => search(e)}>Search</button>
       </form>
     </React.Fragment>
   )
